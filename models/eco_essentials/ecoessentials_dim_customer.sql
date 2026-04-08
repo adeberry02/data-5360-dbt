69ca46a24bc65ff33f15e676d46ac4e0f8918453
Adelaide Berry <aeberry02@gmail.com>
2026-04-08 17:46:05 +0000

    - this is for project deliverable #3

--- models/eco_essentials/ecoessentials_dim_customer.sql
+++ models/eco_essentials/ecoessentials_dim_customer.sql
@@ -5,19 +5,50 @@
 }}
 
 
-SELECT
-{{ dbt_utils.generate_surrogate_key(['customer_id', 'Customer_First_Name', 'Customer_Last_Name']) }} as custkey,
-customer_id,
-Customer_First_Name,
-Customer_Last_Name,
-Customer_Phone,
-Customer_Address,
-Customer_City,
-Customer_State,
-Customer_Zip,
-Customer_Country,
-Customer_Email,
-SubscriberID
-FROM {{ source('emailevents_landing', 'marketingemails') }} me
-JOIN {{source('transactional_landing', 'customer')}} t
-ON me.subscriberfirstname = t.Customer_First_Name AND me.subscriberlastname = t.Customer_Last_Name
+with sales_source as (
+    select
+        customer_id,
+        Customer_First_Name,
+        Customer_Last_Name,
+        Customer_Phone,
+        Customer_Address,
+        Customer_City,
+        Customer_State,
+        Customer_Zip,
+        Customer_Country,
+        Customer_Email,
+    from {{ source('transactional_landing', 'customer') }}
+)
+
+, marketing_emails_source as (
+    select distinct
+        SubscriberID,
+        subscriberfirstname,
+        subscriberlastname,
+        subscriberemail
+    from {{ source('emailevents_landing', 'marketingemails') }}
+)
+
+, final as (
+    select
+        ss.customer_id,
+        coalesce(ss.Customer_First_Name, mes.subscriberfirstname) as firstname,
+        coalesce(ss.Customer_Last_Name, mes.subscriberlastname) as lastname,
+        Customer_Phone,
+        Customer_Address,
+        Customer_City,
+        Customer_State,
+        Customer_Zip,
+        Customer_Country,
+        coalesce(ss.customer_email, mes.subscriberemail) as email,
+        mes.SubscriberID
+    from sales_source ss
+    full join marketing_emails_source mes
+        on ss.Customer_First_Name = mes.subscriberfirstname
+        and ss.Customer_Last_Name = mes.subscriberlastname
+)
+
+select
+    {{ dbt_utils.generate_surrogate_key(['firstname', 'lastname']) }} as custkey,
+    *
+from final
